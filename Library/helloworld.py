3c3fed63074e6c30070ef748f5152a5b9e5cf9df
hvancast <hvancast@terpmail.umd.edu>
2025-10-09 13:22:36 -0400

    Update helloworld.py

--- Library/helloworld.py
+++ Library/helloworld.py
@@ -1,1 +1,21 @@
-print("hello world")
+def salary_input(self):
+    salary = float(input("Enter a salary number: "))
+    tax_rate = float(input("Enter a tax rate (0-1): "))
+    return calculate_money_available_after_tax(salary, tax_rate)
+
+def calculate_money_available_after_tax(self, salary_rate):
+    if salary < 0: 
+        raise ValueError("Salary cannot be a negative.")
+    if not 0 <= tax_rate <=1:
+        raise ValueError("Tax rate must be between 0 and 1")
+    tax_amount= salary * tax_rate
+    available= salary-tax_amount
+    return available 
+
+
+
+
+
+ 
+
+
