c5a93dcee47c2d44288ef4cb8e637246324a4fe4
hvancast <hvancast@terpmail.umd.edu>
2025-10-10 09:26:29 -0400

    Update helloworld.py

--- Library/helloworld.py
+++ Library/helloworld.py
@@ -1,28 +1,18 @@
-def salary_input(self):
-    salary = float(input("Enter a salary number: "))
-    tax_rate = float(input("Enter a tax rate (0-1): "))
-    return calculate_money_available_after_tax(salary, tax_rate)
-
+#Calculates the money available after tax and validates inputs
 def calculate_money_available_after_tax(salary, tax_rate):
     if salary < 0: 
         raise ValueError("Salary cannot be negative.")
     if not 0 <= tax_rate <= 1:
         raise ValueError("Tax rate must be between 0 and 1")
+    #Calculate tax and subtracts from salary to get available amount
     tax_amount = salary * tax_rate
     available = salary - tax_amount
     return available
 
-investments = []
-def add_investment(name, amount, type):
-    investments.append({"name": name, "amount":amount, "type":type})
-
-def get_investments():
-    return investments
-
-
-
-
-
+#Tracks and stores user monthly spending
+def add_monthly_spending(amount, category, description, month):
+    with open("monthly_spending.txt", "a") as file:
+        file.write(f"{month},{amount},{category},{description}\n")
 
 
  
